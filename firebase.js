9f4e8aafdcbf0b9f61e3c6086f16929d1cde399c
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-04-23 19:26:33 -0400

    adds Links, Icons and Images to Navbar, ubar, utilizes BIM naming convention for the JSX elements and styles them accordingly in the Header.css

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBviaKokOdxnzLeNA8WA_hn5W9Mn5pNccw",
+  authDomain: "clone-25b2f.firebaseapp.com",
+  projectId: "clone-25b2f",
+  storageBucket: "clone-25b2f.appspot.com",
+  messagingSenderId: "352416698890",
+  appId: "1:352416698890:web:54429c436b6caf60a5c5f3"
+};
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Header";
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+
+          <Route path="/login">
+            <h1>Login</h1>
+          </Route>
+
+          <Route path="/">
+            <Header />
+            <h1>Home</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
