eff333409fcbe809f0cec3f4b778b52db868d83e
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-04-23 20:35:58 -0400

    updates index.css global styles, updates Home.css styles, adds Product.js component and Product.css, destructures props and utilizes dummy hard-coded data to generate an example of a products list

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Product from "./Product";
 import "./Home.css";
 
 function Home() {
@@ -7,8 +8,21 @@ function Home() {
       <img
         className="home__image"
         src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
-      ></img>
-      <h1> Home Page </h1>
+      />
+      <div className="home__row">
+        <Product id="123" title="123" price={11.96} rating={5} image="" />
+        <Product id="123" title="123" price={11.96} rating={5} image="" />
+      </div>
+
+      <div className="home__row">
+        <Product id="123" title="123" price={11.96} rating={5} image="" />
+        <Product id="123" title="123" price={11.96} rating={5} image="" />
+        <Product id="123" title="123" price={11.96} rating={5} image="" />
+      </div>
+
+      <div className="home__row">
+        <Product id="123" title="123" price={11.96} rating={5} image="" />
+      </div>
     </div>
   );
 }
