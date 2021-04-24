19c83bcc9d0e8bb6423926f466b15d1c3ee6bcde
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-04-24 15:29:45 -0400

    adds CheckoutProduct.js component and associated styles, adds functionality for REMOVE_FROM_BASKET case inside of the reducer.js, creates onClick for the button in out CheckoutProduct component, begins creating the Subtotal component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Header";
 import Home from "./Home";
+import Checkout from "./Checkout";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <div className="app">
         <Switch>
           <Route path="/checkout">
-            <h1>Checkout</h1>
+            <Checkout />
           </Route>
 
           <Route path="/login">
--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,22 @@
 export const initialState = {
-  basket: [],
+  basket: [
+    {
+      id: "3254254345",
+      title: "New Apple iPad Pro",
+      price: 598.99,
+      rating: 4,
+      image:
+        "https://m.media-amazon.com/images/I/81SGb5l+lZL._AC_UY436_FMwebp_QL65_.jpg",
+    },
+    {
+      id: "3254254345",
+      title: "New Apple iPad Pro",
+      price: 598.99,
+      rating: 4,
+      image:
+        "https://m.media-amazon.com/images/I/81SGb5l+lZL._AC_UY436_FMwebp_QL65_.jpg",
+    },
+  ],
   user: null,
 };
 
@@ -11,13 +28,33 @@ const reducer = (state, action) => {
       // Logic for adding item to Basket
       return {
         ...state,
-        basket: [...state.basket, action.item]
+        basket: [...state.basket, action.item],
       };
     case "REMOVE_FROM_BASKET":
       // Logic for removing item from Basket
+
+      // we cloned the basket
+      let newBasket = [...state.basket];
+
+      // we check to see if the product exists
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+
+      if (index >= 0) {
+        // item exists in basket, remove it...
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Cant remove product (id: ${action.id}) as it is not in the basket`
+        );
+      }
+
       return {
         ...state,
+        basket: newBasket,
       };
+
     default:
       return state;
   }
